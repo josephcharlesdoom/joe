4489e93dc9a371b7a94136ddb5adc5925a1155c0
joe ahart <josephcharlesdoom@gmail.com>
2013-03-05 21:23:57 -0600

    created a status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joe::Application.config.secret_token = '1e2ee2e1876b74f00d668df7ae009d260bccc51cd877173543eacf83a86c0582a117c6bb1fd6b2f13cc1fc3bd2fefcef4ae6863c115753630e2a3ae21c38a3b8'
